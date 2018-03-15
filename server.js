05c41d2c90de7a7b0563bfb4f67b7d4151e28210
ValliMariappan <valli241994@gmail.com>
2018-03-15 16:26:17 +0530

    server routing

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-
-app.use(express.static( __dirname+ '/public'));
+app.use('/home'+express.static(__dirname+'/home'));
+app.use('/register'+express.static(__dirname+'/register'));
+app.use('/products'+express.static(__dirname+'/products'));
+app.use(express.static(__dirname+ '/'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
