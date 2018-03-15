5582485caa311ed37b835ba36cb9f1d10128b80f
ValliMariappan <valli241994@gmail.com>
2018-03-15 16:33:07 +0530

    final

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use('/home'+express.static(__dirname+'/home'));
-app.use('/register'+express.static(__dirname+'/register'));
-app.use('/products'+express.static(__dirname+'/products'));
+
+app.use(express.static(__dirname+'/component'));
 app.use(express.static(__dirname+ '/'));
 
 // Start the app by listening on the default Heroku port
