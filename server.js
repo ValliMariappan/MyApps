6bedc5a380b38fd2422bc746dcd535c0c4f3f893
ValliMariappan <valli241994@gmail.com>
2018-03-15 17:33:48 +0530

    PLS

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // Serve only the static files form the dist directory
 
 app.use(express.static(__dirname+'/component'));
-app.use(express.static(__dirname+ '/'));
+//app.use(express.static(__dirname+ '/'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
