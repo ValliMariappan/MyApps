e4f4287b22c10aed41c55cd67cbb6ce1c687179a
ValliMariappan <valli241994@gmail.com>
2018-03-15 16:06:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,9 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080;
+//Install express server
+const express = require('express');
+const app = express();
 
-app.use('/',express.static(__dirname));
+// Serve only the static files form the dist directory
+app.use(express.static( '/'+__dirname));
 
-app.get('/:date',function(req,res){
-  var input = decodeURI(req.params.date);
-  var isNumber = /^\d+$/.test(input);
-  var unix, string;
-  if(isNumber){
-    var t = new Date(parseInt(input));
-    string = t.toUTCString();
-    unix = input;
-  } else {
-    unix = Date.parse(input) || "null";
-    string= Date.parse(input) ? input : "null";
-  }
-
-  var output = {
-    string: string,
-    unix:unix,
-  };
-
-  res.json(output)
-});
-
-app.listen(PORT,function(){
-  console.log('server successfully started on port '+PORT);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
