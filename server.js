1ca2de117e4211d70e4570e8d7069a53a9f1b4ec
ValliMariappan <valli241994@gmail.com>
2018-03-15 15:42:14 +0530

    dfgfg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "/public"));
+app.use('/'+express.static(__dirname));
 
 app.get('/:date',function(req,res){
   var input = decodeURI(req.params.date);
