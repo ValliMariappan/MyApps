f419e349cfd107b805fe1d2d32b8d392f5bea4eb
ValliMariappan <valli241994@gmail.com>
2018-03-20 12:41:44 +0530

    no return

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -8,8 +8,11 @@ interface NodeModule {
 
 interface AppRequire {
   <T>(path: string): T | any;
-  (paths: string[], callback: (...modules: any[]) => void): void | any;
-  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, chunkName?: string) => void | any;
+  (paths: string[], 
+    callback: (...modules: any[]) => void): void | any;
+  ensure: (
+    paths: string[], 
+    callback: (require: <T>(path: string) => T) => void, chunkName?: string) => void | any;
   context: any;
 }
 interface NodeRequire extends AppRequire  {
