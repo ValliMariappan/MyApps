50a8a8b1241d9c6ab9d9faeeeec37925c6b74b35
ValliMariappan <valli241994@gmail.com>
2018-03-20 11:51:35 +0530

    noderequire

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,7 +1,18 @@
 /* SystemJS module definition */
 
 declare var module: NodeModule;
-
+declare var require : NodeRequire;
 interface NodeModule {
   id: string;
 }
+
+interface AppRequire {
+  <T>(path: string): T | any;
+  (paths: string[], callback: (...modules: any[]) => void): void | any;
+  ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, chunkName?: string) => void | any;
+  context: any;
+}
+interface NodeRequire extends AppRequire  {
+
+}
+
